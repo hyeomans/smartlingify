5233d7e6c3aa366840091682fbc2eb9cf3e76be5
Hector Yeomans <yeomans.leonel@gmail.com>
2016-02-03 15:39:55 -0800

    Adding recurse files promise

---
+++ src/recurse-files.js
@@ -0,0 +1,133 @@
+'use strict';
+/* global module */
+var format      = require('stringformat');
+var filewalker  = require('filewalker');
+var fs          = require('fs');
+var Promise     = require('bluebird');
+var access      = Promise.promisify(fs.access);
+
+var smartlingFileUri = 'https://api.smartling.com/v1/file/get?apiKey={0}&fileUri={1}&projectId={2}';
+var smartlingSiblingFileUri = 'https://api.smartling.com/v1/file/get?apiKey={0}&fileUri={1}&projectId={2}&locale={3}';
+var smartlingImport = 'https://api.smartling.com/v1/file/import';
+
+var apiKey;
+var projectId;
+
+module.exports = function (options) {
+    if(!options['smartlingApiKey'] || !options['smartlingProjectId']) {
+        throw new Error('Smartling Api key and Project Id are required.');
+    }
+
+    if(!options.path || !options.extension || !options.locales || options.locales.length === 0) {
+        throw new Error('Path, extension and locales are needed');
+    }
+
+    apiKey = options.smartlingApiKey;
+    projectId = options.smartlingProjectId;
+
+    return function() {
+        return recurseDirectory(options, ignoreFilesRegex(options.locales, options.extension))
+            .then((files) => appendSiblings(files, options))
+            .then(removeNonExistentSiblings);
+    };
+};
+
+function removeNonExistentSiblings(files) {
+    return Promise.all(Promise.map(files, (file) => checkSiblingsOfRoot(file)));
+}
+
+function checkSiblingsOfRoot(file) {
+    return Promise.all(Promise.map(file.siblings, checkFile))
+        .then((siblings) => siblings.filter((sibling) => !!sibling))
+        .then((siblings) => {
+            file.siblings = siblings;
+            return file;
+        });
+}
+
+function checkFile(file) {
+    return access(file.absolutePath, fs.R_OK | fs.W_OK) //Read&Write permissions
+        .then(() => file)
+        .catch(() => void(0));
+}
+
+function appendSiblings(files, options) {
+    files.forEach((file) => {
+        var siblings = [];
+        file.siblings = siblings;
+        options.locales.reduce((prev, locale) => {
+            var fullPathSplitted = file.absolutePath.split(options.extension);
+            var relativePathSplitted = file.relativePath.split(options.extension);
+            var smartlingPath = format('./{0}', file.relativePath);
+            prev.push({
+                absolutePath: format('{0}{1}.{2}', fullPathSplitted[0], locale, options.extension),
+                relativePath: format('{0}{1}.{2}', relativePathSplitted[0], locale, options.extension),
+                fileUri: format(smartlingSiblingFileUri, apiKey, smartlingPath, projectId, locale),
+                upload: {
+                    uri: smartlingImport,
+                    params: [
+                        ['apiKey', apiKey],
+                        ['projectId', projectId],
+                        ['fileUri', smartlingPath],
+                        ['locale', locale],
+                        ['overwrite', true],
+                        ['translationState', 'PUBLISHED'],
+                        ['fileType', options.extension]
+                    ]
+                }
+            });
+            return prev;
+        }, siblings);
+    });
+    return files;
+}
+
+function recurseDirectory(options, matchRegex) {
+    var params = {
+        maxPending: 10,
+        matchRegExp: matchRegex
+    };
+
+    var files = [];
+    return new Promise(function (resolve, reject) {
+        filewalker(options.path, params)
+            .on('file', function(p, s, a) {
+                var smartlingPath = format('./{0}', p);
+                files.push({
+                    absolutePath: a,
+                    relativePath: p,
+                    fileUri: format(smartlingFileUri, apiKey, smartlingPath, projectId),
+                    upload: {
+                        uri: 'https://api.smartling.com/v1/file/upload',
+                        params: [
+                            ['apiKey', apiKey],
+                            ['projectId', projectId],
+                            ['fileUri', smartlingPath],
+                            ['fileType', options.extension]
+                        ]
+                    }
+                });
+            })
+            .on('error', function(err) {
+                reject(err);
+            })
+            .on('done', function() {
+                resolve(files);
+            })
+            .walk();
+    });
+}
+
+function ignoreFilesRegex(supportedLocales, fileExtension) {
+    var ignoreLocales = supportedLocales.map(function (locale, index) {
+        if(index === 0) {
+            return '('+ locale + '|';
+        }
+        if(index === supportedLocales.length -1) {
+            return locale + ')';
+        }
+        return locale + '|';
+    });
+    var ignoreRegex = format('^(?!.*\\.{0}\\.{1}).*\\.{1}$', ignoreLocales.join(''), fileExtension);
+    return new RegExp(ignoreRegex, 'g');
+}
---
+++ test/recurse/recurse-files.spec.js
@@ -0,0 +1,64 @@
+'use strict';
+/* global it, describe, beforeEach */
+let chai = require('chai');
+let expect = chai.expect;
+let path = require('path');
+let options;
+let recurseFiles;
+
+describe('Recurse Files', () => {
+    it('should fail if smartling values are not provided', () => {
+        expect(() => {
+            require(path.join('..', '..', 'src', 'recurse-files'))({});
+        }).to.throw('Smartling Api key and Project Id are required.');
+    });
+
+    it('should fail if values are not provided', () => {
+        expect(() => {
+            require(path.join('..', '..', 'src', 'recurse-files'))({smartlingApiKey: 'apiKey', smartlingProjectId: 'projectId'});
+        }).to.throw('Path, extension and locales are needed');
+    });
+
+    describe('Fill up files array', () => {
+        beforeEach(() => {
+            options = {
+                path: path.join('.', 'test', 'recurse', 'sample'),
+                extension: 'resx',
+                locales: [ 'en-AU', 'en-GB', 'fr-CA', 'de-DE', 'ja-JP', 'es-MX' ],
+                smartlingApiKey: 'mockApi',
+                smartlingProjectId: 'mockProject'
+            };
+
+            recurseFiles = require(path.join('..', '..', 'src', 'recurse-files'))(options);
+        });
+
+        it('result should get desired interface', () => {
+            let result = recurseFiles();
+            expect(result).to.eventually.have.deep.property('[0].absolutePath');
+            expect(result).to.eventually.have.deep.property('[0].upload');
+            expect(result).to.eventually.have.deep.property('[0].relativePath');
+            return expect(result).to.eventually.have.deep.property('[0].fileUri');
+        });
+
+        it('should retrieve siblings of root files', () => {
+            let result = recurseFiles();
+            expect(result).to.eventually.have.deep.property('[0].siblings');
+            expect(result).to.eventually.have.deep.property('[0].siblings[0].absolutePath');
+            return expect(result).to.eventually.have.deep.property('[0].siblings[0].upload');
+        });
+
+        it('should remove non-existent siblings', () => {
+            let result = recurseFiles();
+            return result.then((files) => {
+                //Given arrays can swith order we have to verify each one.
+                for(var file = 0; file < files.length; file += 1) {
+                    if(files[file].relativePath === 'complete/Index.cshtml.resx') {
+                        expect(files[file]['siblings']).to.have.length(6);
+                    } else {
+                        expect(files[file]['siblings']).to.have.length(2);
+                    }
+                }
+            });
+        });
+    });
+});
